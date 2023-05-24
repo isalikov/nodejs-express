1fce5f8a2e54e65f47ea47f99756ad6e3136c77b
Iakov Salikov <salikovpro@gmail.com>
2023-05-24 12:00:05 +0100

    fix: cleanup js files

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var NotFound_1 = require("../middlewares/NotFound");
-var routes = (0, express_1.Router)();
-routes.use('*', NotFound_1.default);
-exports.default = routes;
